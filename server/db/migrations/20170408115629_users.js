ebe2748aa61d84bf0d09fe006ec981a1354da04d
Evan Moore <etmoore@gmail.com>
2017-04-13 12:59:09 -0600

    Create Users migration

--- server/db/migrations/20170408115629_users.js
+++ server/db/migrations/20170408115629_users.js
@@ -2,9 +2,9 @@ exports.up = (knex) => (
   knex.schema.createTable('users', (table) => {
     table.increments('id')
     table.string('email').notNullable()
-    table.string('password_hash').notNullable()
-    table.string('first_name').notNullable()
-    table.string('last_name').notNullable()
+    table.string('password').notNullable()
+    table.string('firstName').notNullable()
+    table.string('lastName').notNullable()
     table.timestamps(true, true)
   })
 )
