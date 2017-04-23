93a673355e6f4cc0e29ba34a02d0d60b3fb16349
Michael Herman <hermanmu@gmail.com>
2017-04-23 15:16:21 -0600

    updated ask view

--- client/src/components/AskView.jsx
+++ client/src/components/AskView.jsx
@@ -6,11 +6,11 @@ class AskView extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      title: 'new title',
-      description: 'new description',
-      start: moment().format('YYYY-MM-DDTHH:mm'),
-      end: moment().add(2, 'hours').format('YYYY-MM-DDTHH:mm'),
-      location: 'location, pl'
+      title: null,
+      description: null,
+      start: null,
+      end: null,
+      location: null
     }
     this.getAsk = this.getAsk.bind(this)
   }
@@ -22,25 +22,28 @@ class AskView extends Component {
   getAsk (askID) {
     axios.get(`http://localhost:3000/api/v1/asks/${askID}`)
       .then((res) => {
-        const {title, description, start, end, location} = res.data
+        console.log(res);
+        const {title, description, start, end, location, user_id} = res.data
         this.setState({
           title,
           description,
           start: moment(start).format('YYYY-MM-DDTHH:mm'),
           end: moment(end).format('YYYY-MM-DDTHH:mm'),
-          location
+          location,
+          user_id,
         })
       })
       .catch(err => console.log(err))
   }
   render () {
-    const {title, description, start, end, location} = this.state
+    const {title, description, start, end, location, user_id} = this.state
     return (
       <div>
         <h1>{title}</h1>
         <p>{description}</p>
         <p><span>{start}</span><span>{end}</span></p>
         <p>{location}</p>
+        <p><span>Created by </span>{user_id}</p>
       </div>
     )
   }
