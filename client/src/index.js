492219aff35c7f523e3b502bbddca1a6d1257aee
Evan Moore <etmoore@gmail.com>
2017-04-30 22:20:47 -0600

    Send auth token with all requests if present
    Configure axios to send the auth token with all requests if it's present
    in localstorage.
    
    closes #52

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import App from './App.jsx'
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
+
+if (window.localStorage.authToken) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${window.localStorage.authToken}`
+} else {
+  delete axios.defaults.headers.common['Authorization']
+}
 
 const Foo = (
   <Router>
